9973404dfadd795dc84aee3930399b91175a5015
vennylee <78582073+vennylee@users.noreply.github.com>
2021-05-16 11:11:25 +0900

    [JS] time, greeting

---
+++ README.md
@@ -0,0 +1,3 @@
+#this is to Do list. i made it using by javascript.
+it has some useful functions showing current time, checking your schedule,
+showing the current position and temperature with bringing your current position(longitude and langitude). 
---
+++ greeting.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const nameForm = document.querySelector(".js-nameForm"),
+nameInput = nameForm.querySelector(".js-nameInput"),
+greeting = document.querySelector(".js-greeting");
+
+
+const NAME_LS = "name";
+const SHOWING_CL = "showing";
+
+function handleSubmit(event){
+event.preventDefault();
+const currentValue = nameInput.value;
+localStorage.setItem(NAME_LS, currentValue);
+    nameInput.value = '';
+}
+
+function askForName(){
+    nameForm.classList.add("showing");
+    greeting.classList.remove("showing");
+}
+
+function paintGreeting(text){
+       nameForm.classList.remove(SHOWING_CL);
+greeting.classList.add(SHOWING_CL);
+greeting.innerText = `Hi ${text}`;
+}
+
+function loadName(){
+const loadedName = localStorage.getItem(NAME_LS);
+if(loadedName === null){
+    askForName();
+}else{
+    paintGreeting(loadedName);
+}
+}
+
+function init(){
+loadName();
+nameForm.addEventListener("submit", handleSubmit);
+}   
+init();

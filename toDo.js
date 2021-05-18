3404d815cece729434c9e4a77fbbca03c459492b
venny <hyungsup42134@gmail.com>
2021-05-18 22:57:07 +0900

    [JS] todo

--- toDo.js
+++ toDo.js
@@ -5,46 +5,67 @@ toDoInput = toDoForm.querySelector(".js-toDoInput"),
 toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = "todos";
-const TODOS = [];
+let TODOS_ARRAY = [];
+let idNumbers = 1;
 
-function saveTodos(obj){
-    localStorage.setItem(TODOS_LS, JSON.stringify(obj));
+function saveToDo(obj){
+localStorage.setItem(TODOS_LS, JSON.stringify(obj));
 }
 
-function paintTodo(text){
-const li = document.createElement('li');
-const delBtn = document.createElement('button');
-delBtn.innerText="☑️";
-const span = document.createElement('span');
-span.innerText=text;
-li.id=TODOS.length;
-li.appendChild(delBtn);
-li.appendChild(span);
-toDoList.appendChild(li);
-const toDosObj = {
+function deleteTodo(e){
+    const btn = e.target;
+    const li = btn.parentElement;
+    toDoList.removeChild(li);
+    const cleanTodo = TODOS_ARRAY.filter(function(todo){
+        return todo.id !== parseInt(li.id)
+    });
+    TODOS_ARRAY=cleanTodo;
+    saveToDo(cleanTodo);
+}
+
+function paintToDo(text){
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    const delBtn = document.createElement("button");
+    const newId = idNumbers;
+    idNumbers = idNumbers + 1
+
+    span.textContent = text;
+    delBtn.textContent = '❌';
+    delBtn.addEventListener('click', deleteTodo);
+    
+    li.appendChild(span);
+    li.appendChild(delBtn);
+    toDoList.appendChild(li);
+li.id=newId;
+const todosObj = {
     text : text,
-    id : TODOS.length
+    id : newId
 }
-TODOS.push(toDosObj);
-saveTodos(TODOS);
-
+TODOS_ARRAY.push(todosObj);
+saveToDo(TODOS_ARRAY);
 }
 
-function handleSubmit(event){
-    event.preventDefault();
+function handleSubmit(e){
+    e.preventDefault();
     const currentValue = toDoInput.value;
+    paintToDo(currentValue);
     toDoInput.value='';
-    paintTodo(currentValue);
+    
 }
+
 
 function loadTodos(){
     const loadedTodos = localStorage.getItem(TODOS_LS);
 if(loadedTodos !== null){
-    console.log(loadedTodos);
     const parsedTodos = JSON.parse(loadedTodos);
-    console.log(parsedTodos);
+   parsedTodos.forEach(function(todo){
+paintToDo(todo.text);
+   });
+
+};
 }
-}
+
 
 function init(){
     loadTodos();
